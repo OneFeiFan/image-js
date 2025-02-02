3fc0649bebbd11324e0ed04b9fb77411b6ecebda
OneFeiFan <3022504358@qq.com>
2025-02-02 17:34:34 +0800

    refactor(browser): 使用uni.request替换OkHttp请求

--- src/browser/environment.js
+++ src/browser/environment.js
@@ -15,44 +15,33 @@ export function createCanvas(width, height) {
   canvas.height = height;
   return canvas;
 }
+
 export function fetchBinary(url, { withCredentials = false } = {}) {
   return new Promise(function (resolve, reject) {
-    if (typeof plus !== 'undefined' && plus.android && typeof plus.android.importClass === 'function') {
+    if (uni !== undefined && typeof uni.request === 'function') {
       try {
-        // 获取 OkHttpClient 类
-        const OkHttpClient = plus.android.importClass("okhttp3.OkHttpClient");
-        const Request = plus.android.importClass("okhttp3.Request");
-        plus.android.importClass("okhttp3.Response");
-        plus.android.importClass("okhttp3.RealCall");
-        plus.android.importClass("okhttp3.ResponseBody");
-
-        // 创建 OkHttpClient 实例
-        const client = new OkHttpClient();
-        // 创建请求对象
-        const request = new Request.Builder().url(url).build();
-        // 发起请求并获取响应
-        const response = client.newCall(request).execute();
-        // 获取响应的二进制数据
-        const byteArray = response.body().bytes();
-
-        // 将 byte[] 转为 JavaScript 中的 arraybuffer
-        const arrayBuffer = new ArrayBuffer(byteArray.length);
-        const uint8Array = new Uint8Array(arrayBuffer);
-        for (let i = 0; i < byteArray.length; i++) {
-          uint8Array[i] = byteArray[i];
-        }
-        // 关闭响应体
-        response.body().close();
-
-        // 返回二进制数据
-        resolve(arrayBuffer);
-
-      } catch (error) {
-        console.error("Error in OkHttp request:", error);
-        reject(error);
+        uni.request({
+          url: url, // 请求的URL
+          method: 'GET', // 请求方式
+          responseType: 'arraybuffer', // 响应数据类型为ArrayBuffer
+          withCredentials: withCredentials, // 是否携带凭证（如果需要）
+          success: (res) => {
+            if (res.statusCode !== 200) {
+              reject(res);
+            } else {
+              resolve(res.data); // 通过resolve返回ArrayBuffer数据
+            }
+          },
+          fail: (err) => {
+            reject(err); // 请求失败时调用reject
+          }
+        });
+      } catch (e) {
+        console.error(e); // 捕获并打印错误信息
       }
     }else {
       try {
+        let start = Date.now()
         let xhr = new self.XMLHttpRequest();
         xhr.open('GET', url, true);
         xhr.responseType = 'arraybuffer';
@@ -60,7 +49,10 @@ export function fetchBinary(url, { withCredentials = false } = {}) {
 
         xhr.onload = function (e) {
           if (this.status !== 200) reject(e);
-          else resolve(this.response);
+          else{
+            console.log('图片加载耗时', Date.now() - start);
+            resolve(this.response)
+          };
         };
         xhr.onerror = reject;
         xhr.send();
